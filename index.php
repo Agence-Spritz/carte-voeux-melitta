acdfd8b224897433814611a08d40ff62bc5df310
Sébastien PAQUET <cavallinox@MacBook-Pro-de-Sebastien-2.local>
2017-12-20 18:32:16 +0100

    Création page

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Happy New Year</title>
+    </head>
+    
+<style>
+body {
+margin: 0 !important;
+padding: 0 !important;
+background-color: rgba(168,33,23,1);
+}
+
+.videoContainer 
+{
+    position:absolute;
+    height:100%;
+    width:100%;
+    overflow: hidden;
+    margin: 0 !important;
+    padding: 0 !important;
+}
+
+.videoContainer video 
+{
+    min-width: 100%;
+    min-height: 100%;
+    width: 100%;
+    height: auto;
+    margin: 0 !important;
+    padding: 0 !important;
+}
+</style>
+
+    <body>
+	    <div class="videoContainer">
+		    <video id="backgroundvid" style="" autoplay controls="controls"  poster="img/couverture.jpg">
+			  <source src="video/171220_Voeux_2018_v2.mp4" type="video/mp4" />
+			</video>
+	    </div>
+    
+    </body>
+</html>
+
+
+
+					
+					
